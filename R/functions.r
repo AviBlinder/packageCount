48e3180301066693a648804336360d3e6d0caf9e
Avi Blinder <aviblinder@gmail.com>
2016-11-11 15:35:20 +0200

    some changes

--- R/functions.r
+++ R/functions.r
@@ -1,4 +1,4 @@
-package_stats <- function(from_date,to_date,package_name){
+cran_stats_by_package <- function(from_date,to_date,package_name){
   
   # Here's an easy way to get all the URLs in R
   library(lubridate)
@@ -36,4 +36,32 @@ package_stats <- function(from_date,to_date,package_name){
   
   names(package_stats_df) <- c("pck_date","pck_count")
   package_stats_df
+}
+######################################################################################
+cran_stats_by_day <- function(Date){
+  
+  # Here's an easy way to get all the URLs in R
+  library(lubridate)
+  ##
+  start <- ymd(Date)
+
+  
+  ##Create regex for package name
+  pattern <- "^.{1,},.{1,},.{1,},.{1,},.{1,},(.{1,}),.{1,},.{1,},.{1,}$"
+  pattern
+  urls <- paste0('http://cran-logs.rstudio.com/', year(start), '/', start, '.csv.gz')
+  
+  package_stats <- c()
+  stats_df <- c()
+  tmp <- tempfile()
+  download.file(urls, tmp)
+  unzipped_file <- read.csv( gzfile(tmp), sep="\t",header=TRUE, 
+                               stringsAsFactors=FALSE)
+  rm(tmp)
+  #remove header    
+  unzipped_file1 <- unzipped_file[-1,]
+  #sum all accurances of package
+  packages_found <- sapply(unzipped_file1,function(x) gsub(pattern,"\\1",x))
+  #extract date from first row
+  packages_found_tbl <- sort(table(packages_found),decreasing = TRUE)
 }
--- R/package_count.r
+++ R/package_count.r
@@ -4,7 +4,13 @@ rm(list=ls())
 
 package_name <- "tsSelect"
 from_date <- '2016-10-01'
-to_date <- today()
+to_date <- today() - days(2)
 
-pck <- package_stats(from_date,to_date , package_name)
+source("R/functions.r")
+pck <- cran_stats_by_package(from_date,to_date , package_name)
+
+
+stats_2016_11_01 <- cran_stats_by_day("2016-11-01")
+
+
 
