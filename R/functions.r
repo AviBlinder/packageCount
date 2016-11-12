c13d1853d5fa2162e47517d6a54d85dba025a500
Avi Blinder <aviblinder@gmail.com>
2016-11-12 21:31:01 +0200

    using tidyr::separate instead of regex

--- R/functions.r
+++ R/functions.r
@@ -2,6 +2,7 @@ cran_stats_by_package <- function(from_date,to_date,package_name){
   
   # Here's an easy way to get all the URLs in R
   library(lubridate)
+  library(tidyr)
   ##
   start <- ymd(from_date)
   today <- ymd(to_date)
@@ -10,7 +11,7 @@ cran_stats_by_package <- function(from_date,to_date,package_name){
   years <- year(all_days)
   
   ##Create regex for package name
-  pattern <- paste0(",",package_name,",.{1,},.{1,}$")
+#  pattern <- paste0(",",package_name,",.{1,},.{1,}$")
   
   urls <- paste0('http://cran-logs.rstudio.com/', years, '/', all_days, '.csv.gz')
   
@@ -21,21 +22,26 @@ cran_stats_by_package <- function(from_date,to_date,package_name){
     download.file(urls[i], tmp)
     unzipped_file <- read.csv( gzfile(tmp), sep="\t",header=TRUE, 
                                stringsAsFactors=FALSE)
-    #remove header    
-    unzipped_file1 <- unzipped_file[-1,]
-    #sum all accurances of package
-    sum_pck_found <- sum(grepl(pattern,unzipped_file1))
-    #extract date from first row
-    file_date <- substr(unzipped_file[1,1],1,10)
-    stats_df <- cbind(file_date=file_date,sum_pck_found=sum_pck_found)    
-    package_stats <- rbind(package_stats,stats_df)     
     rm(tmp)
+    #remove header    
+#    unzipped_file1 <- unzipped_file[-1,]
+    #sum all accurances of package
+#    sum_pck_found <- sum(grepl(pattern,unzipped_file1))
+    #extract date from first row
+#    file_date <- substr(unzipped_file[1,1],1,10)
+    names(unzipped_file) <- "ConcatCol"
+    unzipped_file_df <- separate(unzipped_file,ConcatCol,sep = ",",
+                                 into = c("date","time","size","r_version","r_arch","r_os",
+                                          "package_name","package_version","country","ip_id"))
+    unzipped_file_df_pck <- subset(unzipped_file_df,package_name == sel_package_name)
+    package_stats <- rbind(package_stats,unzipped_file_df_pck)     
   }
   #####
-  package_stats_df <- data.frame(package_stats)
+#  package_stats_df <- data.frame(package_stats)
   
-  names(package_stats_df) <- c("pck_date","pck_count")
-  package_stats_df
+#  names(package_stats_df) <- c("pck_date","pck_count")
+#  package_stats_df$pck_count <- as.integer(package_stats_df$pck_count)
+  package_stats
 }
 ######################################################################################
 cran_stats_by_day <- function(Date){
@@ -62,6 +68,34 @@ cran_stats_by_day <- function(Date){
   unzipped_file1 <- unzipped_file[-1,]
   #sum all accurances of package
   packages_found <- sapply(unzipped_file1,function(x) gsub(pattern,"\\1",x))
+  #extract date from first row
+  packages_found_tbl <- sort(table(packages_found),decreasing = TRUE)
+}
+######################################################################################
+cran_stats_by_day_parsed <- function(Date){
+  
+  # Here's an easy way to get all the URLs in R
+  library(lubridate)
+  ##
+  start <- ymd(Date)
+  
+  
+  ##Create regex for package name
+  pattern <- "^.{1,},.{1,},.{1,},.{1,},.{1,},(.{1,}),(.{1,}),(.{1,}),(.{1,})$"
+  pattern
+  urls <- paste0('http://cran-logs.rstudio.com/', year(start), '/', start, '.csv.gz')
+  
+  package_stats <- c()
+  stats_df <- c()
+  tmp <- tempfile()
+  download.file(urls, tmp)
+  unzipped_file <- read.csv( gzfile(tmp), sep="\t",header=TRUE, 
+                             stringsAsFactors=FALSE)
+  rm(tmp)
+  #remove header    
+  unzipped_file1 <- unzipped_file[-1,]
+  #sum all accurances of package
+  packages_found <- sapply(unzipped_file1,function(x) gsub(pattern,"\\1,\\3",x))
   #extract date from first row
   packages_found_tbl <- sort(table(packages_found),decreasing = TRUE)
 }
--- R/package_count.r
+++ R/package_count.r
@@ -2,27 +2,39 @@ library(lubridate)
 
 rm(list=ls())
 
-package_name <- "tsSelect"
+#package_name <- "tsSelect"
+sel_package_name <- "RNeo4j"
 from_date <- '2016-10-01'
-to_date <- today() - days(2)
+to_date <- '2016-10-03'
+#to_date <- today() - days(2)
 
 
 #load functions
 source("R/functions.r")
 ###################################################################
 ##Read range of dates and summarize stats by package
+## --> Stats for One package over a range of dates
+## Returns a data frame with results
 pck <- cran_stats_by_package(from_date,to_date , package_name)
+head(pck)
+countries_table <- table(pck$country)
+countries_table
+
+
 library(knitr)
-kable(pck)
+kable(countries_table)
 
 ###################################################################
 #Read CRAN by specific date
+## --> Stats for All package over One Date
 stats_2016_11_01 <- cran_stats_by_day("2016-11-01")
 
-dates <- seq(ymd("2016-10-06"),ymd("2016-10-08"),by="day")
+
 
 ###################################################################
-##Read by day, but with range of dates 
+#### --> Stats for All packages over a range of dates
+dates <- seq(ymd("2016-10-06"),ymd("2016-10-07"),by="day")
+
 stats_cran <- sapply(dates,function(x) cran_stats_by_day(x))
 
 #Combine results from all extracted days
