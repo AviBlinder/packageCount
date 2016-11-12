9e88275ce8fe16b9b4a37bf9d966c1c100b3ec82
Avi Blinder <aviblinder@gmail.com>
2016-11-12 21:42:53 +0200

    minor changes

--- R/functions.r
+++ R/functions.r
@@ -1,6 +1,5 @@
 cran_stats_by_package <- function(from_date,to_date,package_name){
   
-  # Here's an easy way to get all the URLs in R
   library(lubridate)
   library(tidyr)
   ##
@@ -9,9 +8,6 @@ cran_stats_by_package <- function(from_date,to_date,package_name){
   
   all_days <- seq(start, today, by = 'day')
   years <- year(all_days)
-  
-  ##Create regex for package name
-#  pattern <- paste0(",",package_name,",.{1,},.{1,}$")
   
   urls <- paste0('http://cran-logs.rstudio.com/', years, '/', all_days, '.csv.gz')
   
@@ -23,12 +19,7 @@ cran_stats_by_package <- function(from_date,to_date,package_name){
     unzipped_file <- read.csv( gzfile(tmp), sep="\t",header=TRUE, 
                                stringsAsFactors=FALSE)
     rm(tmp)
-    #remove header    
-#    unzipped_file1 <- unzipped_file[-1,]
-    #sum all accurances of package
-#    sum_pck_found <- sum(grepl(pattern,unzipped_file1))
-    #extract date from first row
-#    file_date <- substr(unzipped_file[1,1],1,10)
+#
     names(unzipped_file) <- "ConcatCol"
     unzipped_file_df <- separate(unzipped_file,ConcatCol,sep = ",",
                                  into = c("date","time","size","r_version","r_arch","r_os",
@@ -36,25 +27,17 @@ cran_stats_by_package <- function(from_date,to_date,package_name){
     unzipped_file_df_pck <- subset(unzipped_file_df,package_name == sel_package_name)
     package_stats <- rbind(package_stats,unzipped_file_df_pck)     
   }
-  #####
-#  package_stats_df <- data.frame(package_stats)
-  
-#  names(package_stats_df) <- c("pck_date","pck_count")
-#  package_stats_df$pck_count <- as.integer(package_stats_df$pck_count)
   package_stats
 }
 ######################################################################################
 cran_stats_by_day <- function(Date){
   
-  # Here's an easy way to get all the URLs in R
   library(lubridate)
   ##
   start <- ymd(Date)
 
   
   ##Create regex for package name
-  pattern <- "^.{1,},.{1,},.{1,},.{1,},.{1,},(.{1,}),.{1,},.{1,},.{1,}$"
-  pattern
   urls <- paste0('http://cran-logs.rstudio.com/', year(start), '/', start, '.csv.gz')
   
   package_stats <- c()
@@ -65,37 +48,8 @@ cran_stats_by_day <- function(Date){
                                stringsAsFactors=FALSE)
   rm(tmp)
   #remove header    
-  unzipped_file1 <- unzipped_file[-1,]
-  #sum all accurances of package
-  packages_found <- sapply(unzipped_file1,function(x) gsub(pattern,"\\1",x))
-  #extract date from first row
-  packages_found_tbl <- sort(table(packages_found),decreasing = TRUE)
-}
-######################################################################################
-cran_stats_by_day_parsed <- function(Date){
-  
-  # Here's an easy way to get all the URLs in R
-  library(lubridate)
-  ##
-  start <- ymd(Date)
-  
-  
-  ##Create regex for package name
-  pattern <- "^.{1,},.{1,},.{1,},.{1,},.{1,},(.{1,}),(.{1,}),(.{1,}),(.{1,})$"
-  pattern
-  urls <- paste0('http://cran-logs.rstudio.com/', year(start), '/', start, '.csv.gz')
-  
-  package_stats <- c()
-  stats_df <- c()
-  tmp <- tempfile()
-  download.file(urls, tmp)
-  unzipped_file <- read.csv( gzfile(tmp), sep="\t",header=TRUE, 
-                             stringsAsFactors=FALSE)
-  rm(tmp)
-  #remove header    
-  unzipped_file1 <- unzipped_file[-1,]
-  #sum all accurances of package
-  packages_found <- sapply(unzipped_file1,function(x) gsub(pattern,"\\1,\\3",x))
-  #extract date from first row
-  packages_found_tbl <- sort(table(packages_found),decreasing = TRUE)
+  names(unzipped_file) <- "ConcatCol"
+  unzipped_file_df <- separate(unzipped_file,ConcatCol,sep = ",",
+                               into = c("date","time","size","r_version","r_arch","r_os",
+                                        "package_name","package_version","country","ip_id"))
 }
--- R/package_count.r
+++ R/package_count.r
@@ -28,42 +28,4 @@ kable(countries_table)
 #Read CRAN by specific date
 ## --> Stats for All package over One Date
 stats_2016_11_01 <- cran_stats_by_day("2016-11-01")
-
-
-
-###################################################################
-#### --> Stats for All packages over a range of dates
-dates <- seq(ymd("2016-10-06"),ymd("2016-10-07"),by="day")
-
-stats_cran <- sapply(dates,function(x) cran_stats_by_day(x))
-
-#Combine results from all extracted days
-
-if (class(stats_cran) == "matrix"){
-  stats_cran_df <- as.data.frame(stats_cran)
-  stats_cran_df$Package <- row.names(stats_cran_df)
-  row.names(stats_cran_df) <- NULL
-  names(stats_cran_df) <- c("Count","Package")
-  stats_cran_df <- stats_cran_df[,c(2,1)]
-  } else{
-  stats_cran_df <- c()
-  for (i in 1:length(stats_cran)){
-    stats_cran_df <- rbind(stats_cran_df,
-                                  as.data.frame(unlist(stats_cran[[i]])))
-  }
-  names(stats_cran_df) <- c("Package","Count")
-}
-  
-
-head(stats_cran_df)
-
-#Summarize the counts of each package
-stats_cran_df_sum <- ddply(stats_cran_df,"Package",summarise, 
-                                  Sum=sum(Count),
-                                  Appearances=length(Count),
-                                  Average=round(mean(Count),0)
-                                  )
-
-kable(head(stats_cran_df_sum,15))
-subset(stats_cran_df_sum,Package==package_name)
-###################################################################
+head(stats_2016_11_01)
