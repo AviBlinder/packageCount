1a496faa27d84e9286b837169b755db7eae81b2d
Avi Blinder <aviblinder.gmail.com>
2016-11-15 10:17:43 +0200

    addition of figures and reorg plots

--- R/package_count.r
+++ R/package_count.r
@@ -1,17 +1,18 @@
 library(lubridate)
 library(dplyr)
-library(plyr)
+#library(plyr)
 library(knitr)
 library(ggplot2)
 
 rm(list=ls())
 
-#package_name <- "tsSelect"
+#package_name <- "sparklyr"
 sel_package_name <- "RNeo4j"
-from_date <- '2016-04-06'
+#sel_package_name <- "sparklyr"
+from_date <- '2016-09-24'
 #to_date <- '2016-11-03'
+##Usually there is a delay of two days in the upload of the latest log
 to_date <- today() - days(2)
-
 
 #load functions
 source("R/functions.r")
@@ -21,45 +22,8 @@ source("R/functions.r")
 ## Returns a data frame with results
 package_stats <- cran_stats_by_package(from_date,to_date , package_name)
 head(package_stats)
-#######
-#Read countries table
-countries <- read.csv("data/GeoLite2-Country-Locations-en.csv",header = TRUE,
-                        stringsAsFactors = FALSE)
-head(countries)
-
-
-package_stats2 <- merge(package_stats,countries,by.x = "country",by.y="country_iso_code") 
-head(package_stats2)
-
-stats1 <- sort(table(package_stats2$country_name),decreasing = TRUE)
-stats1
-
-
-package_stats2 %>% group_by(continent_name) %>%
-                  summarise(count=n()) %>%
-                  arrange(-count) %>%
-                  mutate(cont_order = row_number(),
-                         continent_name = factor(continent_name,
-                                            levels = continent_name[order(cont_order)])) %>%
-                  ggplot(aes(x=continent_name,y=count))+
-                  geom_bar(aes(fill=continent_name),stat="identity",
-                           show.legend = FALSE) +
-                  coord_flip()+
-                  geom_text(aes(label=continent_name),
-                  y=0.2,
-                  hjust=0,
-                  angle=0,
-                  size=4,
-                  color="#222222") +
-                  xlab("Continent Name") +
-                  ylab("Total Number of Package Installed") +
-                  theme(axis.text.y=element_blank(),
-                        axis.ticks.y=element_blank())
-
-
-kable(stats_by_continent)
-
-
+file_name <- paste0("data/",sel_package_name,"_Stats")
+saveRDS(package_stats,file_name)
 
 ###################################################################
 #Read CRAN by specific date
