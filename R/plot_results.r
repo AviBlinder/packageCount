9832f099d6714b9732ff888fd194c6b67a5587c1
Avi Blinder <aviblinder@gmail.com>
2016-11-15 20:10:23 +0200

    comparing two packages

--- R/plot_results.r
+++ R/plot_results.r
@@ -1,9 +1,38 @@
-library(ggplot2)
+#Read countries table
+countries <- read.csv("data/GeoLite2-Country-Locations-en.csv",header = TRUE,
+                      stringsAsFactors = FALSE)
+head(countries)
 
-sjp.setTheme(theme = "539", axis.angle.x = 90)
-ggplot(mydf, aes(x = date.short, y = n)) +
-  geom_bar(stat = "identity", width = .5, alpha = .5, fill = "#3399cc") +
-  scale_y_continuous(expand = c(0, 0), breaks = seq(250, 1500, 250)) +
-  labs(x = sprintf("Monthly CRAN-downloads of sjPlot package since first 
-                   release until 4th March (total download: %i)", 
-                   sum(mydf$n)), y = NULL)
+
+package_stats2 <- merge(package_stats,countries,by.x = "country",by.y="country_iso_code") 
+head(package_stats2)
+
+stats1 <- sort(table(package_stats2$country_name),decreasing = TRUE)
+stats1
+head(package_stats2)
+
+#in case of conflict between plyr and dplyr, detach plyr
+#detach("package:plyr", unload=TRUE) 
+package_stats2 %>% group_by(continent_name)  %>%
+    dplyr::summarise(count=n()) %>%
+    arrange(-count) %>%
+    mutate(cont_order = row_number(),
+           continent_name = factor(continent_name,
+             levels = continent_name[order(cont_order)])) %>%
+    ggplot(aes(x=continent_name,y=count))+
+    geom_bar(aes(fill=continent_name),stat="identity",
+             show.legend = FALSE) +
+    coord_flip()+
+    geom_text(aes(label=continent_name),
+              y=0.2,
+              hjust=0,
+              angle=0,
+              size=4,
+              color="#222222") +
+    xlab("Continent Name") +
+    ylab("Total Number of Package Installed") +
+    theme(axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())
+
+
+#kable(stats_by_continent)
