43933a6bb00f20493691c922b2b7b126c9cd0f46
Avi Blinder <aviblinder@gmail.com>
2016-11-16 21:18:41 +0200

    plot of 3 packages' stats

--- R/plot_results.r
+++ R/plot_results.r
@@ -36,6 +36,39 @@ package_stats2 %>% group_by(continent_name)  %>%
           axis.ticks.y=element_blank())
 
 
+######
 
-#kable(stats_by_continent)
+mongo_stats <- readRDS("./data/mongolite_Stats")
+dim(mongo_stats)
+spark_stats <- readRDS("./data/sparklyr_Stats")
+dim(spark_stats)
+neo_stats <- readRDS("./data/RNeo4j_Stats")
+dim(neo_stats)
+comb_stats <- rbind(mongo_stats,spark_stats,neo_stats)
 
+package_stats2 <- merge(comb_stats,countries,by.x = "country",by.y="country_iso_code") 
+head(package_stats2)
+
+package_stats2 %>% 
+  mutate(package_name = factor(package_name)) %>%
+  group_by(package_name,continent_name)  %>%
+  dplyr::summarise(count=n()) %>%
+  arrange(-count) %>%
+  mutate(cont_order = row_number(),
+         continent_name = factor(continent_name,
+                                 levels = continent_name[order(cont_order)])) %>%
+  ggplot(aes(x=continent_name,y=count))+
+  geom_bar(aes(fill=continent_name),stat="identity",
+           show.legend = FALSE) +
+ coord_flip()+
+  facet_grid(~ package_name) +
+  geom_text(aes(label=continent_name),
+            y=0.2,
+            hjust=0,
+            angle=0,
+            size=4,
+            color="#222222") +
+  xlab("Continent Name") +
+  ylab("Total Number of Package Installed") +
+  theme(axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
